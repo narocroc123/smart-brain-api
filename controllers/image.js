910be34e4415c6a662fb76323728b7c385dcbdf9
narocroc123 <42586341+narocroc123@users.noreply.github.com>
2019-01-29 16:54:18 +0000

    adding project

---
+++ controllers/image.js
@@ -0,0 +1,30 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: 'fea79c0fbc38419384559952cc289443'
+});
+
+const handleApiCall = (req, res) => {
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('Unable To Work With API'));
+}
+
+const handleImagePut = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('Unable To Get Entries'))
+}
+
+module.exports = {
+    handleImagePut,
+    handleApiCall
+}
