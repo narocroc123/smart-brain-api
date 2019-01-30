9458568b516dc53a1bbdfa6f7aba30da47c1ffe8
narocroc123 <42586341+narocroc123@users.noreply.github.com>
2019-01-30 18:16:11 +0000

    adjusting port

--- server.js
+++ server.js
@@ -31,6 +31,10 @@ app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)});
 app.put('/image', (req, res) => {image.handleImagePut(req, res, db)});
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)});
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`app is running on port ${process.env.PORT}`);
+app.listen(3000, () => {
+    console.log('app is running on port 3000');
 });
+
+// app.listen(process.env.PORT || 3000, () => {
+//     console.log(`app is running on port ${process.env.PORT}`);
+// });
